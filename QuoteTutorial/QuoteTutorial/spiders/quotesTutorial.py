be6acb7c1440fced50efa9ed3d13055463e9d882
Anul Jain <anuljain23@gmail.com>
2020-07-15 01:14:47 +0530

    learned to login to a website with scrapy

--- QuoteTutorial/QuoteTutorial/spiders/quotesTutorial.py
+++ QuoteTutorial/QuoteTutorial/spiders/quotesTutorial.py
@@ -1,14 +1,25 @@
 import scrapy
+from scrapy.http import FormRequest
+from scrapy.utils.response import open_in_browser
 from ..items import QuotetutorialItem
 
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
     page_number = 2
     start_urls = [
-        'http://quotes.toscrape.com/page/1/'
+        'http://quotes.toscrape.com/login'
     ]
 
     def parse(self, response):
+        token = response.css('form input::attr(value)').extract()
+        return FormRequest.from_response(response,formdata={
+        'csrf_token':token,
+        'username': 'anuljain23@gmail.com',
+        'password': 'asdfghjkl'
+        },callback=self.start_scraping)
+
+    def start_scraping(self,response):
+        open_in_browser(response)
         items = QuotetutorialItem()
         all_div_quotes = response.css('div.quote')
 
@@ -26,4 +37,4 @@ class QuoteSpider(scrapy.Spider):
         next_page = 'http://quotes.toscrape.com/page/' + str(QuoteSpider.page_number) + '/'
         if QuoteSpider.page_number<11:
             QuoteSpider.page_number += 1
-            yield response.follow(next_page,callback=self.parse)
+            yield response.follow(next_page,callback=self.start_scraping)
