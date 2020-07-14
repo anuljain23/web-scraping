84ac683aaaf926ceaacc09b96bdb615eb028eea6
Anul Jain <anuljain23@gmail.com>
2020-07-14 23:19:21 +0530

    learned to store the scraped data in a database (sqlite3)

--- QuoteTutorial/QuoteTutorial/pipelines.py
+++ QuoteTutorial/QuoteTutorial/pipelines.py
@@ -6,8 +6,34 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import sqlite3
 
 class QuotetutorialPipeline:
+
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = sqlite3.connect("myquotes.db")
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        self.curr.execute("""DROP TABLE IF EXISTS quotes_tb""")
+        self.curr.execute("""CREATE TABLE quotes_tb(
+                        title text,
+                        author text,
+                        tag text
+                        )""")
+
+    def store_db(self, item):
+        self.curr.execute("""INSERT INTO quotes_tb values (?,?,?)""",(
+                        item['title'][0],
+                        item['author'][0],
+                        item['tag'][0],
+        ))
+        self.conn.commit()
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
