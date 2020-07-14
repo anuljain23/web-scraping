6b862dd063f93d85fd0ab49c810e5fa6659ba684
Anul Jain <anuljain23@gmail.com>
2020-07-15 00:39:24 +0530

    learned to scrape multiple pages of a website using pagination method of scrapy

--- QuoteTutorial/QuoteTutorial/spiders/quotesTutorial.py
+++ QuoteTutorial/QuoteTutorial/spiders/quotesTutorial.py
@@ -3,8 +3,9 @@ from ..items import QuotetutorialItem
 
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
+    page_number = 2
     start_urls = [
-        'http://quotes.toscrape.com/'
+        'http://quotes.toscrape.com/page/1/'
     ]
 
     def parse(self, response):
@@ -22,6 +23,7 @@ class QuoteSpider(scrapy.Spider):
 
             yield items
 
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
+        next_page = 'http://quotes.toscrape.com/page/' + str(QuoteSpider.page_number) + '/'
+        if QuoteSpider.page_number<11:
+            QuoteSpider.page_number += 1
             yield response.follow(next_page,callback=self.parse)
